804402c89b128c578d213edf443c095d2d567ba8
Eryk Skalinski <e22rico@gmail.com>
2018-04-13 21:32:53 +0100

    Started Ship Class Layout

--- XWingGameEngine/XWingGameEngine/Form1.cs
+++ XWingGameEngine/XWingGameEngine/Form1.cs
@@ -22,4 +22,89 @@ namespace XWingGameEngine
 
         }
     }
+    static class Upgrades
+    {
+        public enum Elite
+        {
+        }
+        public enum Astromech
+        {
+        }
+        public enum Torpedoes
+        {
+        }
+        public enum Missiles
+        {
+        }
+        public enum Cannon
+        {
+        }
+        public enum Turret
+        {
+        }
+        public enum Bomb
+        {
+        }
+        public enum Crew
+        {
+        }
+        public enum Salvaged_Astromechs
+        {
+        }
+        public enum System
+        {
+        }
+        public enum Title
+        {
+        }
+        public enum Modification
+        {
+        }
+        public enum Illicit
+        {
+        }
+        public enum Cargo
+        {
+        }
+        public enum Hardpoint
+        {
+        }
+        public enum Team
+        {      
+        }
+        public enum Tech
+        {
+        }
+    }
+    static class Ship_Util
+    {
+        public enum Rebel
+        {
+
+        }
+        public enum Imperial
+        {
+
+        }
+        public enum Scum
+        {
+
+        }
+    }
+    abstract class ShipMove
+    {
+        public abstract void Move();
+    }
+    class Ship
+    {
+        ShipMove _shipmove;
+        public string name;
+        
+        Ship(string name)
+        {
+            this.name = name;
+
+        }
+
+    }
 }
